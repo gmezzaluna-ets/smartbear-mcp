8568a1f2b149cab6a9dec51fe7fd21b6df744816
gmezzaluna-ets <gmezzaluna@ets.org>
2025-11-12 09:27:52 -0500

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,23 @@
 #!/usr/bin/env node
+import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
 import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
+import { z } from "zod";
 import { ApiHubClient } from "./api-hub/client.js";
 import { BugsnagClient } from "./bugsnag/client.js";
 import { CollaboratorClient } from "./collaborator/client.js";
-import Bugsnag from "./common/bugsnag.js";
 import { SmartBearMcpServer } from "./common/server.js";
 import { PactflowClient } from "./pactflow/client.js";
 import { QmetryClient } from "./qmetry/client.js";
 import { ReflectClient } from "./reflect/client.js";
 import { ZephyrClient } from "./zephyr/client.js";
+import Bugsnag from "./common/bugsnag.js";
 
-// This is used to report errors in the MCP server itself
-// If you want to use your own BugSnag API key, set the MCP_SERVER_BUGSNAG_API_KEY environment variable
 const McpServerBugsnagAPIKey = process.env.MCP_SERVER_BUGSNAG_API_KEY;
 if (McpServerBugsnagAPIKey) {
   Bugsnag.start(McpServerBugsnagAPIKey);
 }
 
-async function main() {
+async function conditions() {
   const server = new SmartBearMcpServer();
 
   const reflectToken = process.env.REFLECT_API_TOKEN;
@@ -114,11 +114,27 @@ async function main() {
     process.exit(1);
   }
 
-  const transport = new StdioServerTransport();
-  await server.connect(transport);
+  return server;
 }
 
+async function mcp() {
+  const mcp = new McpServer();
+  await conditions().server(mcp);
+}
+
+async function stdio() {
+  const transport = new StdioServerTransport();
+  await conditions().connect(transport);
+}
+
+async function main() {
+  mcp().catch((error) => {
+    console.error("MCP error:", error);
+    stdio().catch((error) => {
+      console.error("STDIO error:", error);
+    });
+  });
+
 main().catch((error) => {
-  console.error("Fatal error in main():", error);
   process.exit(1);
 });
